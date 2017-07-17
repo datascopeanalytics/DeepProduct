a9aa621366d5d5a0bd9040838650d7a547c8d76b
Ross Weijer <ross.weijer@datascopeanalytics.com>
2017-07-17 17:55:04 -0500

    Basic schema for sqlite db to collect user feedback

---
+++ couch_tinder_schema.sql
@@ -0,0 +1,26 @@
+/***************************
+Basic idea behind the schema below
+Every time someone votes yes or no on a pair of images, we record
+their feedback in this table. This will keep track of
+
+	- What browser session someone is voting in
+	- The file path to the first image being shown
+	- The file path to the second image being shown
+	- Which CNN model judged these as similar
+	- How the human voted (perhaps 'yes' they match, 
+		'no' they dont, and an 'idk' option)?
+
+Any of this is subject to clarification/refinement as 
+we get a clearer idea of structure.
+***************************/
+
+
+drop table if exists user_feedback;
+create table user_feedback(
+	id integer primary key autoincrement,
+	session text not null,
+	pair_file_1 text not null,
+	pair_file_2 text not null,
+	model text not null,
+	user_vote
+	);
