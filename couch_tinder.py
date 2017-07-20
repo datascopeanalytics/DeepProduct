ea0319f02d81e30d12f09c4277984a5cbd2f9dd4
Ross Weijer <ross.weijer@datascopeanalytics.com>
2017-07-20 12:09:45 -0500

    interstitial work saving commit

--- couch_tinder.py
+++ couch_tinder.py
@@ -4,7 +4,7 @@ import sqlite3
 import numpy as np
 
 from flask import Flask, request, session, g, redirect, url_for, abort, \
-	render_template, send_from_directory, flash
+	render_template, send_from_directory, flash, current_app
 
 app = Flask(__name__)
 
@@ -64,6 +64,18 @@ def initdb_command():
 	init_db()
 	print('Initialized the database')
 
+'''Assuming we have information to the effect of "this model
+recommended that these images go together" in some source 
+text file, this makes sure that the current application
+context has a representation of those pairs
+'''
+def access_model_pairs():
+	if not hasattr(g, 'model_pairs'):
+		with app.open_resource('web_app_AB_pairs.txt', mode = 'r') as f:
+			g.model_pairs = f.readlines()
+	return g.model_pairs
+
+
 ############	END QUESTIONABLY USEFUL DATABASE FUNCTIONS 	 ##################
 
 @app.route('/')
@@ -78,21 +90,23 @@ def home(n_jpgs = 4):
 	return render_template('index.html', data = data)
 
 
-@app.route('/models/', defaults = {'model_name':None, 'pair_name':None})
-@app.route('/models/<model_name>/<pair_name>/')
-def models(model_name = None, pair_name = None):
+@app.route('/models', defaults = {'served_pair':None})
+# If we're always going to be choosing randomly, not sure
+# if this defaults option is even necessary
+def models(served_pair = None):
 
-	# For now, routing to models without specifying a model and
-	# pair dumps you back home instead...
-	if not model_name and not pair_name:
-		model_name = np.random.choice(app.config['MODELS'],1).item()
-		pair_name = np.random.choice(app.config['PAIRS'], 1).item()
-		return redirect(url_for('models', model_name = model_name, pair_name = pair_name))
+	if not served_pair:
+		all_pairs = access_model_pairs()
+		served_pair = np.random.choice(all_pairs, 1).item()
+		# FIX THIS STUFF.....
+		pair_model = served_pair.split()[0]
+		pair_img_1 = os.path.join(app.config['DEEP_FASHION_IMAGES'], served_pair.split()[1])
+		pair_img_2 = os.path.join(app.config['DEEP_FASHION_IMAGES'], served_pair.split()[2])
+		#return redirect(url_for('models', model_name = model_name, pair_name = pair_name))
 
-	data = {'model_name':model_name,
-	        'pair_name':pair_name,
-	        'image_1_path':f'models/{model_name}/pairs/{pair_name}/img1.jpg',
-	        'image_2_path':f'models/{model_name}/pairs/{pair_name}/img2.jpg'
+	data = {'model_served':pair_model,
+	        'image_1_path':'/'.join(pair_img_1.split('/')[1:]),
+	        'image_2_path':'/'.join(pair_img_2.split('/')[1:])
 	        }
 
 	return render_template('models.html', data = data)
@@ -109,3 +123,9 @@ def judgement():
 		else:
 			flash("You said those didn't match")
 	return redirect(url_for('models'))
+
+# if __name__ == '__main__':
+# 	with app.open_resource('web_app_AB_pairs.txt', mode = 'r') as f:
+# 		model_pairs = f.readlines()
+# 		import pdb
+# 		pdb.set_trace()
