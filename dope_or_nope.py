3b29a004601f16446eba9d1ff62405c65b8b9d14
Ross Weijer <ross.weijer@datascopeanalytics.com>
2017-08-08 09:19:53 -0500

    Two model layout deprecated for now

--- dope_or_nope.py
+++ dope_or_nope.py
@@ -221,32 +221,6 @@ def demo(pair_idx):
 	return render_template('demo.html', data = data)
 
 
-
-
-
-
-'''Testing a layout where we display three images, potentially with
-two models supplying a recommendation for the same base image
-
-Right now, I'm just choosing three images at random, but we can just
-as well pass in images directly from a text file
-'''
-# @app.route('/twomodels')
-# def twomodels():
-# 	src_dir = os.path.join(app.config['DEEP_FASHION_IMAGES'],'img')
-# 	random_subdir = os.path.join(src_dir, np.random.choice(os.listdir(src_dir),1).item())
-# 	sampled = np.random.choice(os.listdir(random_subdir), size = 3, replace = False)
-# 	sampled = ['/'.join(os.path.join(random_subdir,v).split('/')[1:]) for v in sampled]
-
-# 	data = {'reference_image': sampled[0],
-# 			'model_left': 'MD1',
-# 			'image_left': sampled[1],
-# 			'model_right': 'MD2',
-# 			'image_right': sampled[2]
-# 			}
-# 	return render_template('twomodels.html', data = data)
-
-
 @app.route('/judgement', methods=['POST'])
 def judgement():
 	if request.method == 'POST':
